ace37dc70e7f7306828814f0d77b6514814a7c71
Paul <pauljanvelyan1712@gmail.com>
2024-01-10 02:13:30 +0300

    Готовая лаба

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ import Dynamic_Structures;
 using namespace Dynamic_Structures;
 // Функция, демонстрирующая работу со стеками, унаследованными от базового класса
 template <typename T>
-void demonstrateAbstractStructure(AbstractClassStack<T>& structure, const std::initializer_list<T>& elements) {
+void demonstrate(AbstractClassStack<T>& structure, const std::initializer_list<T>& elements) {
     std::cout << "Добавление элементов в структуру: ";
     for (const auto& element : elements) {
         structure.Push(element);
@@ -32,11 +32,11 @@ int main() {
 
     std::cout << "\n" << "Стек на основе однонаправленного списка" << "\n";
     Dynamic_Structures::StackBasedOnUnidirectionalLinkedList<int> intStack;
-    demonstrateAbstractStructure(intStack, { 1, 2, 3, 4, 5});
+    demonstrate(intStack, { 1, 2, 3, 4, 5});
 
     std::cout << "\n" <<"Стек на основе двунаправленного циклического списка" << "\n";
     Dynamic_Structures::StackBasedOnUnidirectionalLinkedList<double> doubleStack;
-    demonstrateAbstractStructure(doubleStack, { 1.1, 2.2, 3.3, 4.4, 5.5 });
+    demonstrate(doubleStack, { 1.1, 2.2, 3.3, 4.4, 5.5 });
 
 	return 0;
 }
